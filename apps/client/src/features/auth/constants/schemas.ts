d266767364d475aaad84f9fe40c8682ad0eb7a7b
Colpno <gvinhh@gmail.com>
2026-04-07 01:45:57 +0700

    refactor (client): fix SignupForm mismatch  birthdate's type

--- apps/client/src/features/auth/constants/schemas.ts
+++ apps/client/src/features/auth/constants/schemas.ts
@@ -1,10 +1,13 @@
 import type { AuthAPIs } from "@repo/types";
-import { isDate } from "date-fns";
 import z, { ZodType } from "zod";
 
 export const signupFormSchema = z.object({
   email: z.email(),
   password: z.string().min(6),
-  birthdate: z.string().nonempty().refine(isDate),
-}) satisfies ZodType<AuthAPIs.Register["body"]>;
+  birthdate: z.date(),
+}) satisfies ZodType<
+  Omit<AuthAPIs.Register["body"], "birthdate"> & {
+    birthdate: Date;
+  }
+>;
 export type SignupFormData = z.infer<typeof signupFormSchema>;
