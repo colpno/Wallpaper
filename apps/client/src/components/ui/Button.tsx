f030217f0b4c4859efee04b7e133d59168654339
Colpno <gvinhh@gmail.com>
2026-04-14 17:48:08 +0700

    feat (client): add toggle variation to Button

--- apps/client/src/components/ui/Button.tsx
+++ apps/client/src/components/ui/Button.tsx
@@ -1,7 +1,34 @@
-import { Button as UIButton } from "@repo/ui/components";
+import type { VariantProps } from "class-variance-authority";
 
-function Button({ type = "button", ...props }: React.ComponentProps<typeof UIButton>) {
-  return <UIButton {...props} type={type} />;
+import { Button as UIButton, buttonVariants, Toggle, toggleVariants } from "@repo/ui/components";
+
+type ButtonAsButtonProps = {
+  toggle?: never;
+} & React.ComponentProps<typeof UIButton>;
+
+type ButtonAsToggleProps = {
+  toggle: boolean;
+} & Omit<React.ComponentProps<typeof Toggle>, keyof VariantProps<typeof toggleVariants>> &
+  Pick<ButtonAsButtonProps, keyof VariantProps<typeof buttonVariants>>;
+
+type Props = ButtonAsButtonProps | ButtonAsToggleProps;
+
+function Button({ type = "button", toggle, ...props }: Props) {
+  if (toggle) {
+    const { size, variant, ...rest } = props as ButtonAsToggleProps;
+
+    return (
+      <UIButton asChild size={size} variant={variant}>
+        <Toggle
+          {...rest}
+          className="data-[state=on]:border-selected-background data-[state=on]:bg-selected-background data-[state=on]:text-selected-foreground! data-[state=on]:hover:border-selected-hover-background data-[state=on]:hover:bg-selected-hover-background"
+          type={type}
+        />
+      </UIButton>
+    );
+  }
+
+  return <UIButton {...(props as ButtonAsButtonProps)} type={type} />;
 }
 
 export default Button;
